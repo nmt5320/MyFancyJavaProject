a08d4e257f0f180da1274b1776fb4b398d799c1a
Farhana <fafroz0804.256@gmail.com>
2022-01-08 19:21:18 +0600

    Implemented Singleton pattern

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -1,15 +1,26 @@
-public class MyFancyJavaProject {
+class MyFancyJavaProject {
+  Private static MyFancyJavaProject MFJP;
+  private MyFancyJavaProject(int fisrt, int second)
+  {
+    this.first = fisrt;
+    this.second = second;
+  }
+   System.out.println("Enter two numbers");
+  first = 10;
+  second = 20;
 
-  public static void main(String[] args) {
-    
-    System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
-    
     System.out.println(first + " " + second);
 
-    // add two numbers
-    int sum = first + second;
+  // add two numbers
+  int sum = first + second;
     System.out.println("The sum is: " + sum);
-  }
 }
+
+  public static getMFJP(int fisrt, int second) {
+
+    if(MFJP == null)
+      MFJP = new MyFancyJavaProject(fisrt,second)
+    return MFJP;
+
+
+  }
