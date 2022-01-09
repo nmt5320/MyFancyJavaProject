9693756051f5984574484c37574a005801501875
tamim <nirob280794@gmail.com>
2022-01-09 23:34:40 +0600

    MyFancyJavaProject added

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -9,7 +9,14 @@ public class MyFancyJavaProject {
     System.out.println(first + " " + second);
 
     // add two numbers
-    int sum = first + second;
+    int sum = adder(first,second);
     System.out.println("The sum is: " + sum);
   }
+
+  public static int adder(int n1, int n2) {
+      int sum;
+      sum = n1 + n2;
+
+      return sum; 
+   }
 }
